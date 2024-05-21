23674ef089cf5b200af8077385b9a3110d26b80a
Ksenia Shurina <kseniashurina18@gmail.com>
2024-05-21 20:10:51 +0200

    Migrations regarding authors

--- BookHub.Domain/Entities/Author.cs
+++ BookHub.Domain/Entities/Author.cs
@@ -12,6 +12,10 @@ namespace BookHub.Domain.Entities
         [MaxLength(100)]
         public string LastName { get; set; } = null!;
 
+        public DateTime DateOfBirth { get; set; }
+
+        public DateTime DateOfDeath { get; set; }
+
         [MaxLength(1000)]
         public string Description { get; set; } = null!;
         public List<Book> Books { get; set; } = null!;
---
+++ BookHub.Infrastructure/Migrations/20240521180726_AddPropertiesToAuthor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookHub.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropertiesToAuthor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Authors",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfDeath",
+                table: "Authors",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfDeath",
+                table: "Authors");
+        }
+    }
+}
